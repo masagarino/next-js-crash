a8a7e65a52b3341d3356bf0d8fc22bc442afa88f
masagarino <marcsagarino19@gmail.com>
2020-02-19 16:09:52 -0800

    first commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from "../component/layout";
+
+const About = () => (
+  <div>
+    <Layout>
+      <div>About Awesome OS Web Dev!!!</div>
+      <p>Web Dev are all awesome!!!</p>
+    </Layout>
+  </div>
+);
+
+export default About;
